5d18ccfe733c5e99bb4df7929cb510d4f90c3b55
Doug Kintop <dkintop@hotmail.com>
2019-09-25 12:49:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,13 +18,7 @@ extend  Memorable
     @@songs
   end
 
-  #def self.reset_all
-   # self.all.clear
-  #end
-
-  #def self.count
-   # self.all.count
-  #end
+ 
 
   def artist=(artist)
     @artist = artist
